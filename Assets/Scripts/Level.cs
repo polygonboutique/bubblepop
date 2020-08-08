f72de47399c5503d0b83f1e97dc92d189bd932b7
Sergej Schefer <sergej.schefer@web.de>
2020-08-08 14:30:28 +0200

    clean up, doing circle collision

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,6 +5,7 @@ using System.Numerics;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Plane = UnityEngine.Plane;
+using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
 public class Level : MonoBehaviour
@@ -38,11 +39,12 @@ public class Level : MonoBehaviour
                 SpawnBallOnGrid(x, y, Ball.GenerateRandomValue());
             }
         }
+
+        ;
         
-        _boundsPlanes = new Plane[3];
-        _boundsPlanes[0] = new Plane(Vector3.right, 15.0f);
-        //  m_Cube.transform.position = hitPoint;
-        
+        _boundsPlanes = new Plane[2];
+        _boundsPlanes[0] = new Plane(Vector3.right, 0.0f); // left
+        _boundsPlanes[1] = new Plane(Vector3.left, _ballSpawner.GeneratePosition(MAX_GRID_WIDTH, 0).magnitude); // right
     }
 
     private void SetupCamera(GameObject mainCamera)
@@ -80,43 +82,9 @@ public class Level : MonoBehaviour
         Destroy(_grid[x, y].gameObject);
     }
 
-    void Update()
+
+    private bool HitBalls(Ray ray, out GameObject gameObject)
     {
-        Vector3 mouseCoordsWorldSpace = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mouseCoordsWorldSpace.z = 0;
-        Vector3 shootDirection = (mouseCoordsWorldSpace - _ballShooter.GetPosition()).normalized;
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-            _ballShooter.ShootBall(shootDirection);
-            _ballShooter.ReloadBall();
-        }
-        
-        
-        
-        Color col = new Color32(255, 0, 22, 255);
-        Ray ray = new Ray(_ballShooter.GetPosition(), shootDirection);
-        Debug.DrawLine(_ballShooter.GetPosition(), _ballShooter.GetPosition() + shootDirection * 100, col);
-
-        float enter = 0.0f;
-        Plane plane = _boundsPlanes[0];
-        
-        Color planeColor = new Color32(0, 255, 255, 255);
-        Vector3 planeOrigin = -plane.normal * plane.distance;
-        Debug.DrawLine(planeOrigin, planeOrigin + Vector3.down * 200, planeColor);
-        Debug.DrawLine(planeOrigin, planeOrigin + Vector3.up * 200, planeColor);
-        
-        
-        if (plane.Raycast(ray, out enter))
-        {
-            Vector3 hitPoint = ray.GetPoint(enter);
-
-            _ballShooter.GetCurrentBall().transform.position = hitPoint;
-            
-            Vector3 reflect = Vector3.Reflect(ray.direction, plane.normal);
-            Debug.DrawLine(hitPoint, hitPoint + reflect * 100, col);
-        }
-
         // RaycastHit hitInfo;
         for (int y = 0; y < MAX_GRID_HEIGHT; ++y) {
             for (int x = 0; x < MAX_GRID_WIDTH; ++x)
@@ -127,15 +95,70 @@ public class Level : MonoBehaviour
                 }
 
                 GameObject go = _grid[x, y].gameObject;
-                
-               
+
+                if (go.GetComponent<Collider>().Raycast(ray, out RaycastHit hitInfo, 100.0f))
+                {
+                    Debug.Log(hitInfo);
+                    Debug.Log(go);
+                    Debug.Log("----");
+                }
+
             }
         }
+        
+        gameObject = null;
+        return false;
+    }
 
-        // Physics.Raycast()
-        // Physics.c
-        // attach first to grid,
-        // play bubble effect on active and it's neighbors, 
-        // check neighbors of active ball on grid to see, if can merge
+    void Update()
+    {
+        Vector3 mouseCoordsWorldSpace = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseCoordsWorldSpace.z = 0;
+        Vector3 shootDirection = (mouseCoordsWorldSpace - _ballShooter.GetPosition()).normalized;
+        Ray ray = new Ray(_ballShooter.GetPosition(), shootDirection);
+        
+        GameObject hitBall;
+        if(!HitBalls(ray, out hitBall))
+        {
+            // Hit planes
+            Color32[] planeColor = new Color32[2];
+            planeColor[0] = new Color32(255, 0, 255, 255);
+            planeColor[1] = new Color32(0, 255, 0, 255);
+        
+            for (int i = 0; i < _boundsPlanes.Length; ++i)
+            {
+                Plane plane = _boundsPlanes[i];
+            
+                Color col = new Color32(255, 0, 22, 255);
+                Debug.DrawLine(_ballShooter.GetPosition(), _ballShooter.GetPosition() + shootDirection * 100, col);
+
+                Vector3 planeOrigin = -plane.normal * plane.distance;
+                Debug.DrawLine(planeOrigin, planeOrigin + Vector3.down * 200, planeColor[i]);
+                Debug.DrawLine(planeOrigin, planeOrigin + Vector3.up * 200, planeColor[i]);
+
+                if (plane.Raycast(ray, out float enter))
+                {
+                    Vector3 hitPoint = ray.GetPoint(enter);
+                
+                    _ballShooter.GetCurrentBall().transform.position = hitPoint;
+                
+                    Vector3 reflect = Vector3.Reflect(ray.direction, plane.normal);
+                    Debug.DrawLine(hitPoint, hitPoint + reflect * 100, col);
+                
+                    HitBalls(ray, out hitBall);
+                }
+            }
+        }
+        
+        if (hitBall)
+        {
+            Debug.Log("Hit a ball, trace ray with tweens, until we reach end!\n");
+                
+            if (Input.GetButtonDown("Fire1"))
+            {
+                _ballShooter.ShootBall(shootDirection);
+                _ballShooter.ReloadBall();
+            }
+        }
     }
 }
